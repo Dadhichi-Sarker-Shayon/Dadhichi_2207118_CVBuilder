97560d7deb6f501fad396129478033b9a58ffc2d
Dadhichi-Sarker-Shayon <www.dadhichipk123@gmail.com>
2025-11-14 17:23:16 +0600

    making cv

---
+++ CVBuilder/src/main/java/Applications/model/CV.java
@@ -0,0 +1,24 @@
+package application;
+public class CV {
+    private String fullname;
+    private String email;
+    private String phone;
+    private String address;
+    private String education;
+    private String skills;
+    private String workexperience;
+    private String project;
+
+    public CV(String fullname,String email,String phone,String address,String education,String skills,String workexperience,String project )
+    {
+        this.fullname=fullname;
+        this.email=email;
+        this.phone=phone;
+        this.address=address;
+        this.education=education;
+        this.skills=skills;
+        this.workexperience=workexperience;
+        this.project=project;
+
+    }
+}
